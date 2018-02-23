b7ebc8dcd19b2c22307008c8302dc4444e833ae9
streakerjake <jake.e.bullard@gmail.com>
2018-02-23 07:51:29 -0700

    get to the choppa

--- libavcodec/niceenc.c
+++ libavcodec/niceenc.c
@@ -1,5 +1,8 @@
 /*
  * NICE image format encoder
+ *
+ * BMP encoder modified by Jacob Bullard and Jocee Porter for CS 3505 FFMPEG project 2018 Spring
+ *
  * Copyright (c) 2006, 2007 Michel Bardiaux
  * Copyright (c) 2009 Daniel Verkamp <daniel at drv.nu>
  *
@@ -27,17 +30,11 @@
 #include "nice.h"
 #include "internal.h"
 
-static const uint32_t monoblack_pal[] = { 0x000000, 0xFFFFFF };
-static const uint32_t rgb565_masks[]  = { 0xF800, 0x07E0, 0x001F };
-static const uint32_t rgb444_masks[]  = { 0x0F00, 0x00F0, 0x000F };
-
-static av_cold int nice_encode_init(AVCodecContext *avctx){
-  if(avctx->pix_fmt == AV_PIX_FMT_RGB8)
+static av_cold int nice_encode_init(AVCodecContext *avctx)
+{
+    // Because our only supported Pixel Format is AV_PIX_FMT_RGB8,
+    // we can set our bits_per_coded_sample directly to 8
     avctx->bits_per_coded_sample = 8;
-  else {
-        av_log(avctx, AV_LOG_INFO, "unsupported pixel format\n");
-        return AVERROR(EINVAL);
-   }
 
     return 0;
 }
@@ -45,118 +42,60 @@ static av_cold int nice_encode_init(AVCodecContext *avctx){
 static int nice_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
                             const AVFrame *pict, int *got_packet)
 {
+    // Variables needed for encoding
     const AVFrame * const p = pict;
-    int n_bytes_image, n_bytes_per_row, n_bytes, i, n, header_size, ret;
-    const uint32_t *pal = NULL;
-    uint32_t palette256[256];
-    int pad_bytes_per_row, pal_entries = 0, compression = NICE_RGB;
+    int n_bytes_per_row, header_size, ret, n_bytes_image, n_bytes;
     int bit_count = avctx->bits_per_coded_sample;
     uint8_t *ptr, *buf;
 
+    // Code that was deemed necessary by professor
 #if FF_API_CODED_FRAME
 FF_DISABLE_DEPRECATION_WARNINGS
-  avctx->coded_frame->pict_type = AV_PICTURE_TYPE_I;
+    avctx->coded_frame->pict_type = AV_PICTURE_TYPE_I;
     avctx->coded_frame->key_frame = 1;
 FF_ENABLE_DEPRECATION_WARNINGS
 #endif
-    avctx->pix_fmt = AV_PIX_RGB8;
- bit_count = 8;
-//switch (avctx->pix_fmt) {
-//   case AV_PIX_FMT_RGB8:
-//  case AV_PIX_FMT_BGR8:
-//  case AV_PIX_FMT_RGB4_BYTE:
-//  case AV_PIX_FMT_BGR4_BYTE:
-//   case AV_PIX_FMT_GRAY8: 
-//av_assert1(bit_count == 8); 
-       //         avpriv_set_systematic_pal2(palette256, avctx->pix_fmt); 
-       // pal = palette256; 
-//       break; 
-//  }
-    if (pal && !pal_entries) pal_entries = 1 << bit_count;
+
+    // Set our header size and number of bytes per row
     n_bytes_per_row = ((int64_t)avctx->width * (int64_t)bit_count + 7LL) >> 3LL;
-    pad_bytes_per_row = (4 - n_bytes_per_row) & 3;
-    n_bytes_image = avctx->height * (n_bytes_per_row + pad_bytes_per_row);
-
-    // STRUCTURE.field refer to the MSVC documentation for BITMAPFILEHEADER
-    // and related pages.
-    //#define SIZE_BITMAPFILEHEADER 14
-    //#define SIZE_BITMAPINFOHEADER 40
-    //JoCee added '+2' to the header size to account for the extra letter added in the header.
-    //header_size = (SIZE_BITMAPFILEHEADER + SIZE_BITMAPINFOHEADER + (pal_entries << 2));
-    printf("before adding 2: %d\n", header_size);
-    //header_size = header_size + 2;
     header_size = 12;
-    printf("after adding 2: %d\n", header_size);
+
+    // The following code is used for error checking and allocating memory
+    n_bytes_image = avctx->height * n_bytes_per_row;
     n_bytes = n_bytes_image + header_size;
-
     if ((ret = ff_alloc_packet2(avctx, pkt, n_bytes, 0)) < 0)
-       return ret;
+        return ret;
 
+    // Set our buffer to be the size of the data in our packet and 
+    // then fill our header will all the information that we will 
+    // need in the decoder
     buf = pkt->data;
-    bytestream_put_byte(&buf, 'N');                   // BITMAPFILEHEADER.bfType
-    bytestream_put_byte(&buf, 'I');                   // do.
+    bytestream_put_byte(&buf, 'N');                   
+    bytestream_put_byte(&buf, 'I');                   
     bytestream_put_byte(&buf, 'C');
     bytestream_put_byte(&buf, 'E');
-    //bytestream_put_le32(&buf, n_bytes);               // BITMAPFILEHEADER.bfSize
-    //bytestream_put_le16(&buf, 0);                     // BITMAPFILEHEADER.bfReserved1
-    //bytestream_put_le16(&buf, 0);                     // BITMAPFILEHEADER.bfReserved2
-    //bytestream_put_le32(&buf, header_size);                 // BITMAPFILEHEADER.bfOffBits
-    //bytestream_put_le32(&buf, SIZE_BITMAPINFOHEADER); // BITMAPINFOHEADER.biSize
-    bytestream_put_le32(&buf, avctx->width);          // BITMAPINFOHEADER.biWidth
-    bytestream_put_le32(&buf, avctx->height);         // BITMAPINFOHEADER.biHeight
-    //bytestream_put_le16(&buf, 1);                     // BITMAPINFOHEADER.biPlanes
-    //bytestream_put_le16(&buf, bit_count);             // BITMAPINFOHEADER.biBitCount
-    //bytestream_put_le32(&buf, compression);           // BITMAPINFOHEADER.biCompression
-    //bytestream_put_le32(&buf, n_bytes_image);         // BITMAPINFOHEADER.biSizeImage
-    //bytestream_put_le32(&buf, 0);                     // BITMAPINFOHEADER.biXPelsPerMeter
-    //bytestream_put_le32(&buf, 0);                     // BITMAPINFOHEADER.biYPelsPerMeter
-    //bytestream_put_le32(&buf, 0);                     // BITMAPINFOHEADER.biClrUsed
-    //bytestream_put_le32(&buf, 0);                     // BITMAPINFOHEADER.biClrImportant
-    
-    //here is where it is inserting the pallette table
-    //for (i = 0; i < pal_entries; i++)
-    //    bytestream_put_le32(&buf, pal[i] & 0xFFFFFF);
+    bytestream_put_le32(&buf, avctx->width);
+    bytestream_put_le32(&buf, avctx->height);
 
-
-    // BMP files are bottom-to-top so we start from the end...
+    // Prepare the picture data buffer and the buffer
+    // used to write to file
     ptr = p->data[0] + (avctx->height - 1) * p->linesize[0];
     buf = pkt->data + header_size;
-    //printf("Number of bytes per row: %d", n_bytes_per_row);
-    //printf("\n");
     
-    
+    // While we have rows to traverse, 
+    // traverse them
+    for(int i = 0; i < avctx->height; i++) 
+    {
+        // Perform a byte-for-byte copy from
+        // our picture data buffer into the 
+        // buffer used as storage for our file
+        memcpy(buf, ptr, n_bytes_per_row);
 
-    for(i = 0; i < avctx->height; i++) {
+	// Increment our buffer index address
+	buf += n_bytes_per_row;
 
-      //Why does bit_count == 16 even matter?
-       // if (bit_count == 16) {
-       //     const uint16_t *src = (const uint16_t *) ptr;
-      //    uint16_t *dst = (uint16_t *) buf;
-      //    for(n = 0; n < avctx->width; n++)
-      //      {
-      //        AV_WL16(dst + n, src[n]);
-		
-      //      }
-      // } 
-      //else {
-            memcpy(buf, ptr, n_bytes_per_row);
-       } 
-       
-	//printf("Height: %d", i);
-	//printf("\n");
-	//for( int i = 0 ; i < n_bytes_per_row; i++)
-	  //{
-	//unsigned char byte = *((unsigned char *)&buf + i);
-	//unsigned char sourcebyte = *((unsigned char *)&ptr + i);
-	//printf("Buffer Byte %d = %u\n", i , (unsigned)byte);
-	//printf("Source Byte %d = %u\n", i , (unsigned)sourcebyte);
-    //}
-
-        buf += n_bytes_per_row;
-	//TODO:  Take out this buffer!  Find out the pad bytes per row size.
-        memset(buf, 0, pad_bytes_per_row);
-        buf += pad_bytes_per_row;
-        ptr -= p->linesize[0]; // ... and go back
+	// Decrement our picture data buffer index address
+        ptr -= p->linesize[0];  
     }
 
     pkt->flags |= AV_PKT_FLAG_KEY;
@@ -164,14 +103,16 @@ FF_ENABLE_DEPRECATION_WARNINGS
     return 0;
 }
 
-AVCodec ff_nice_encoder = {
+AVCodec ff_nice_encoder = 
+{
     .name           = "nice",
     .long_name      = NULL_IF_CONFIG_SMALL("NICE image (a project for CS 3505)"),
     .type           = AVMEDIA_TYPE_VIDEO,
     .id             = AV_CODEC_ID_NICE,
     .init           = nice_encode_init,
     .encode2        = nice_encode_frame,
-    .pix_fmts       = (const enum AVPixelFormat[]){
+    .pix_fmts       = (const enum AVPixelFormat[])
+    {
          AV_PIX_FMT_RGB8,
         AV_PIX_FMT_NONE
     },
